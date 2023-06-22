86b0630fef9a108a348afd8d0797a4bde8da6a7d
Lawrence Good <lawrence.good55@gmail.com>
2023-06-22 14:31:22 -0700

    Updated global types

--- global-env.d.ts
+++ global-env.d.ts
@@ -1,7 +1,14 @@
 export declare global {
+  export type Role = 'admin' | 'user';
+  /** Supabase user */
   export type User = {
-    id: string
-    email: string
-    name: string
-  }
+    id: string;
+    email: string;
+    name: string;
+    role: Role;
+    created_at: string;
+    updated_at: string;
+  };
+
+  export type Variant = 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link';
 }
