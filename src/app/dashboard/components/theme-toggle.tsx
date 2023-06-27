d2236f102c56ecfcf70fbdd92d93e18ed9891308
Lawrence Good <lawrence.good55@gmail.com>
2023-06-26 21:59:27 -0700

    Remove color transition on theme toggler

--- src/app/dashboard/components/theme-toggle.tsx
+++ src/app/dashboard/components/theme-toggle.tsx
@@ -14,10 +14,13 @@ export function ThemeToggle() {
       size="icon"
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
-      <Sun size={22} className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Sun
+        size={22}
+        className="rotate-0 scale-100 transition-transform dark:-rotate-90 dark:scale-0"
+      />
       <Moon
         size={22}
-        className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        className="absolute rotate-90 scale-0 transition-transform dark:rotate-0 dark:scale-100"
       />
       <span className="sr-only">Toggle theme</span>
     </Button>
