5f5f51d84510d074b92439f550d23bcbf5699662
Lawrence Good <lawrence.good55@gmail.com>
2023-06-26 21:59:07 -0700

    Update temp redirect for personal-finance route

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { withSentryConfig } = require('@sentry/nextjs');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -11,6 +12,23 @@ const nextConfig = {
         source: '/dashboard',
         destination: '/dashboard/home',
         permanent: true,
+      },
+      {
+        source: '/dashboard/personal-finance',
+        destination: '/dashboard/personal-finance/wealth-accelerator-info',
+        permanent: false,
+      },
+    ];
+  },
+  rewrites: async () => {
+    return [
+      {
+        // Proxy /sentry to Sentry
+        source: '/sentry/:path*',
+        // If in CI, use a placeholder URL
+        destination: process.env.CI
+          ? '/sentry'
+          : `https://${process.env.SENTRY_DSN.split('@')[1].split('/')[0]}/:path*`,
       },
     ];
   },
