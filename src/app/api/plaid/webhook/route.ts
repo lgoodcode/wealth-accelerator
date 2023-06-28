4e5428de350a50284d5b11d10defedd806275b5a
Lawrence Good <lawrence.good55@gmail.com>
2023-06-27 22:19:58 -0700

    Testing webhook

--- src/app/api/plaid/webhook/route.ts
+++ src/app/api/plaid/webhook/route.ts
@@ -1,10 +1,28 @@
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
-  console.log('/api/plaid/webhook POST');
+  const body = await req.json().catch(() => null);
 
-  return NextResponse.json({
-    error: null,
-    data: 'OK',
-  });
+  if (!body) {
+    return NextResponse.json({ error: 'Missing body' }, { status: 400 });
+  }
+
+  console.log('/api/plaid/webhook POST', body);
+
+  const { webhook_code, item_id } = body;
+
+  switch (webhook_code) {
+    case 'DEFAULT_UPDATE':
+      break;
+
+    case 'SYNC_UPDATES_AVAILABLE': {
+      console.log('trigger sync');
+      break;
+    }
+
+    default:
+      console.log('unhandled webhook_code', webhook_code);
+  }
+
+  return NextResponse.json({ success: true });
 }
