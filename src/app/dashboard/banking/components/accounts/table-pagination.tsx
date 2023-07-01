31545ea034c3f31fdbe52b899e1520f61bbb402e
Lawrence Good <lawrence.good55@gmail.com>
2023-07-01 16:43:04 -0700

    Add rows list

--- src/app/dashboard/banking/components/accounts/table-pagination.tsx
+++ src/app/dashboard/banking/components/accounts/table-pagination.tsx
@@ -17,14 +17,13 @@ interface TablePaginationProps {
 
 export function TablePagination({ table }: TablePaginationProps) {
   return (
-    // <div className="flex items-center justify-between px-2">
-    // Because we are not using the selection column, we can remove the first div and justify-end the second div
-    <div className="flex items-center justify-end px-2">
-      {/* This shows the number of rows selected if using the selection column */}
-      {/* <div className="flex-1 text-sm text-muted-foreground">
-        {table.getFilteredSelectedRowModel().rows.length} of{' '}
-        {table.getFilteredRowModel().rows.length} row(s) selected.
-      </div> */}
+    <div className="flex items-center justify-between px-2">
+      <div className="flex-1 text-sm text-muted-foreground">
+        Displaying{' '}
+        {table.getState().pagination.pageSize * table.getState().pagination.pageIndex + 1} through{' '}
+        {table.getState().pagination.pageSize * (table.getState().pagination.pageIndex + 1)} of{' '}
+        {table.getFilteredRowModel().rows.length} row(s)
+      </div>
       <div className="flex items-center space-x-6 lg:space-x-8">
         <div className="flex items-center space-x-2">
           <p className="text-sm font-medium">Rows per page</p>
