5a37ee61c94cf7abbdd6e9d24014f4b54f499cf9
Lawrence Good <lawrence.good55@gmail.com>
2023-07-03 21:03:51 -0700

    refactor

--- src/app/dashboard/banking/components/transactions/table-pagination.tsx
+++ src/app/dashboard/banking/components/transactions/table-pagination.tsx
@@ -43,13 +43,13 @@ export function TablePagination({ table }: TablePaginationProps) {
         <div className="flex items-center space-x-2">
           <p className="text-sm font-medium">Rows per page</p>
           <Select
-            value={`${table.getState().pagination.pageSize}`}
+            value={`${paginationSize}`}
             onValueChange={(value) => {
               table.setPageSize(Number(value));
             }}
           >
             <SelectTrigger className="h-8 w-[70px]">
-              <SelectValue placeholder={table.getState().pagination.pageSize} />
+              <SelectValue placeholder={paginationSize} />
             </SelectTrigger>
             <SelectContent side="top">
               {[10, 20, 30, 40, 50].map((pageSize) => (
@@ -61,7 +61,7 @@ export function TablePagination({ table }: TablePaginationProps) {
           </Select>
         </div>
         <div className="flex min-w-[100px] items-center justify-center text-sm font-medium">
-          Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+          Page {paginationIndex + 1} of {table.getPageCount()}
         </div>
         <div className="flex items-center space-x-2">
           <Button
