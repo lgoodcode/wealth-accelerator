af646fafd993b77dd0544374e444d6fd1365e0d7
Lawrence Good <lawrence.good55@gmail.com>
2023-07-03 17:07:47 -0700

    Test naming route handlers

--- src/app/api/plaid/institutions/remove/[item_id]/route.ts
+++ src/app/api/plaid/institutions/remove/[item_id]/route.ts
@@ -1,55 +1,63 @@
-import { NextResponse } from 'next/server';
-import { captureException } from '@sentry/nextjs';
+const testFunc = () => {
+  throw new Error('test');
+};
 
-import { getUser } from '@/lib/supabase/server/getUser';
-import { createSupabase } from '@/lib/supabase/server/createSupabase';
-import { plaidClient } from '@/lib/plaid/config';
-import { getItemFromItemId } from '@/lib/plaid/getItemFromItemId';
-
-interface RemoveInstitutionParams {
-  params: {
-    item_id: string;
-  };
+export default function DELETE() {
+  testFunc();
 }
 
-export async function DELETE(_: Request, { params: { item_id } }: RemoveInstitutionParams) {
-  const user = await getUser();
+// import { NextResponse } from 'next/server';
+// import { captureException } from '@sentry/nextjs';
 
-  if (!user) {
-    return NextResponse.json({ error: 'No user found' }, { status: 401 });
-  }
+// import { getUser } from '@/lib/supabase/server/getUser';
+// import { createSupabase } from '@/lib/supabase/server/createSupabase';
+// import { plaidClient } from '@/lib/plaid/config';
+// import { getItemFromItemId } from '@/lib/plaid/getItemFromItemId';
 
-  // Verify the parameters
-  if (!item_id) {
-    return NextResponse.json({ error: 'Missing item_id' }, { status: 400 });
-  }
+// interface RemoveInstitutionParams {
+//   params: {
+//     item_id: string;
+//   };
+// }
 
-  const supabase = createSupabase();
-  const { error: itemError, data: item } = await getItemFromItemId(item_id);
+// export async function DELETE(_: Request, { params: { item_id } }: RemoveInstitutionParams) {
+//   const user = await getUser();
 
-  if (itemError) {
-    console.error(itemError);
-    captureException(itemError);
-    return NextResponse.json({ error: 'Failed to retrieve item' }, { status: 500 });
-  }
+//   if (!user) {
+//     return NextResponse.json({ error: 'No user found' }, { status: 401 });
+//   }
 
-  // Remove the item from the database
-  const { error: supabaseError } = await supabase.from('plaid').delete().eq('item_id', item_id);
+//   // Verify the parameters
+//   if (!item_id) {
+//     return NextResponse.json({ error: 'Missing item_id' }, { status: 400 });
+//   }
 
-  if (supabaseError) {
-    console.error(supabaseError);
-    captureException(supabaseError);
-    return NextResponse.json({ error: 'Failed to remove item' }, { status: 500 });
-  }
+//   const supabase = createSupabase();
+//   const { error: itemError, data: item } = await getItemFromItemId(item_id);
 
-  // Revoke the access token
-  try {
-    await plaidClient.itemRemove({ access_token: item.access_token });
-  } catch (error) {
-    console.error(error);
-    captureException(error);
-    return NextResponse.json({ error: 'Failed to remove item' }, { status: 500 });
-  }
+//   if (itemError) {
+//     console.error(itemError);
+//     captureException(itemError);
+//     return NextResponse.json({ error: 'Failed to retrieve item' }, { status: 500 });
+//   }
 
-  return NextResponse.json({ success: true });
-}
+//   // Remove the item from the database
+//   const { error: supabaseError } = await supabase.from('plaid').delete().eq('item_id', item_id);
+
+//   if (supabaseError) {
+//     console.error(supabaseError);
+//     captureException(supabaseError);
+//     return NextResponse.json({ error: 'Failed to remove item' }, { status: 500 });
+//   }
+
+//   // Revoke the access token
+//   try {
+//     await plaidClient.itemRemove({ access_token: item.access_token });
+//   } catch (error) {
+//     console.error(error);
+//     captureException(error);
+//     return NextResponse.json({ error: 'Failed to remove item' }, { status: 500 });
+//   }
+
+//   return NextResponse.json({ success: true });
+// }
