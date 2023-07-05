5278c3fb7f7ce568bb4807242301a592edb55c93
Lawrence Good <lawrence.good55@gmail.com>
2023-07-05 16:46:58 -0700

    added Accordion

--- src/app/dashboard/(finance)/creative-cash-flow/page.tsx
+++ src/app/dashboard/(finance)/creative-cash-flow/page.tsx
@@ -1,8 +1,35 @@
+import { redirect } from 'next/navigation';
+import { captureException } from '@sentry/nextjs';
+import type { Metadata } from 'next';
+
+import { createSupabase } from '@/lib/supabase/server/createSupabase';
+import { getUser } from '@/lib/supabase/server/getUser';
+import { PageError } from '@/components/page-error';
+import { Separator } from '@/components/ui/separator';
+
+export const metadata: Metadata = {
+  title: 'Creative Cash Flow',
+};
+
 export default async function CreativeCashFlowPage() {
+  const user = await getUser();
+
+  if (!user) {
+    redirect('/login');
+  }
+
   return (
-    <div className="space-y-2">
-      <h3 className="text-xl font-medium">Personal Finance</h3>
-      <p className="text-muted-foreground">Select a section to manage.</p>
+    <div className="p-8">
+      <div className="space-y-1">
+        <h2 className="text-3xl font-bold">Banking</h2>
+        <p className="text-muted-foreground">
+          Manage institutions and view transactions for your bank accounts.
+        </p>
+      </div>
+      <Separator className="mt-6" />
+      <>
+        <div></div>
+      </>
     </div>
   );
 }
