613cc2c2ee3eab8afcf1a65cc2e8dd726235ce38
Lawrence Good <lawrence.good55@gmail.com>
2023-07-10 14:10:35 -0700

    pagination down

--- src/app/dashboard/admin/manage-users/components/table/table-pagination.tsx
+++ src/app/dashboard/admin/manage-users/components/table/table-pagination.tsx
@@ -50,7 +50,7 @@ export function TablePagination({ table }: TablePaginationProps) {
             <SelectTrigger className="h-8 w-[70px]">
               <SelectValue placeholder={paginationSize} />
             </SelectTrigger>
-            <SelectContent side="top">
+            <SelectContent side="bottom">
               {[10, 20, 30, 40, 50].map((pageSize) => (
                 <SelectItem key={pageSize} value={`${pageSize}`}>
                   {pageSize}
