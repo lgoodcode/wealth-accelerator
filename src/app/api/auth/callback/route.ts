9c5b5bc2fe517a2bd2cbcc225fa4072e71bb67ed
Lawrence Good <lawrence.good55@gmail.com>
2023-07-14 21:54:55 -0700

    Error handling for auth code exchange

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,6 +1,7 @@
 import { cookies } from 'next/headers';
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { NextResponse } from 'next/server';
+import { captureException } from '@sentry/nextjs';
 
 export const GET = exchangeCodeForSession;
 
@@ -8,9 +9,14 @@ async function exchangeCodeForSession(request: Request) {
   const requestURL = new URL(request.url);
   const code = requestURL.searchParams.get('code');
 
-  if (code) {
-    const supabase = createRouteHandlerClient({ cookies });
-    await supabase.auth.exchangeCodeForSession(code);
+  try {
+    if (code) {
+      const supabase = createRouteHandlerClient({ cookies });
+      await supabase.auth.exchangeCodeForSession(code);
+    }
+  } catch (error) {
+    console.error(error);
+    captureException(error);
   }
 
   return NextResponse.redirect(`${requestURL.origin}/login`);
