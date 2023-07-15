86a2acdf5b18e6da16ffc897d35ff42159e1b749
Lawrence Good <lawrence.good55@gmail.com>
2023-07-15 11:27:31 -0700

    Update light shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ export const theme = {
     },
     boxShadow: {
       'white-md':
-        '0 4px 6px -1px rgba(255, 255, 255, 0.1), 0 2px 4px -2px rgba(255, 255, 255, 0.1)',
+        '0 3px 5px -1px rgba(255, 255, 255, 0.1), 0 2px 4px -2px rgba(255, 255, 255, 0.1)',
     },
     colors: {
       border: 'hsl(var(--border))',
