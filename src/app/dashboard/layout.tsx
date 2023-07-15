72eea60da05bce99fb4204f68c8e10dbb99a3f31
Lawrence Good <lawrence.good55@gmail.com>
2023-07-15 12:48:39 -0700

    Update layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,6 +8,8 @@ import { Header } from './components/header';
 import { QueryProvider } from './components/query-provider';
 import { ToastProvider } from './components/toast-provider';
 
+const HEADER_HEIGHT = 65;
+
 export const metadata: Metadata = {
   description: 'Wealth Accelerator app.',
   // Disable indexing for all pages that use this layout. This is useful for
@@ -43,8 +45,14 @@ export default async function DashboardLayout({ children }: DashboardLayoutProps
     >
       <UserProvider user={user}>
         <QueryProvider>
-          <Header />
-          <div className="absolute -z-10 opacity-40 h-full w-full hidden dark:flex flex-col flex-grow bg-right-top bg-no-repeat bg-[url('/img/bg-gradient.svg')]" />
+          <Header height={HEADER_HEIGHT} />
+          <div
+            className="absolute -z-10 opacity-40 w-full hidden dark:flex flex-col flex-grow bg-right-top bg-no-repeat"
+            style={{
+              height: `calc(100% - ${HEADER_HEIGHT}px)`,
+              backgroundImage: 'url(/img/bg-gradient.svg)',
+            }}
+          />
           {children}
         </QueryProvider>
         <ToastProvider />
