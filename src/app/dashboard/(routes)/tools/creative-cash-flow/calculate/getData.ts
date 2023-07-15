81f28f0e562e2a6973260a3f5f02dd24d316042a
Lawrence Good <lawrence.good55@gmail.com>
2023-07-15 10:59:32 -0700

    refactor data fetching

---
+++ src/app/dashboard/(routes)/tools/creative-cash-flow/calculate/getData.ts
@@ -0,0 +1,59 @@
+import { createSupabase, type SupabaseServer } from '@/lib/supabase/server/createSupabase';
+import { Transaction } from '@/lib/plaid/types/transactions';
+
+const getTransactions = async (supabase: SupabaseServer, user_id: string) => {
+  // Get all of the users transactions data split into business and personal
+  const { error: transactionsError, data: transactionsData } = await supabase.rpc(
+    'get_transactions_by_user_id',
+    {
+      arg_user_id: user_id,
+    }
+  );
+
+  if (transactionsError || !transactionsData) {
+    throw transactionsError || new Error('No transactionsData returned');
+  }
+
+  return transactionsData as {
+    business: Transaction[];
+    personal: Transaction[];
+  };
+};
+
+const getCollections = async (supabase: SupabaseServer, user_id: string) => {
+  // Get the users year to date collections value to add to the Year to Date results
+  const { error: collectionsError, data: collectionsData } = await supabase
+    .from('personal_finance')
+    .select('ytd_collections')
+    .eq('user_id', user_id)
+    .single();
+
+  if (collectionsError || !collectionsData) {
+    throw collectionsError || new Error('No collectionsData returned');
+  }
+
+  return collectionsData.ytd_collections;
+};
+
+export const getData = async (user_id: string) => {
+  try {
+    const supabase = createSupabase();
+    const [transactions, ytd_collections] = await Promise.all([
+      getTransactions(supabase, user_id),
+      getCollections(supabase, user_id),
+    ]);
+
+    return {
+      error: null,
+      data: {
+        transactions,
+        ytd_collections,
+      },
+    };
+  } catch (error) {
+    return {
+      error: error as Error,
+      data: null,
+    };
+  }
+};
--- src/app/dashboard/(routes)/tools/creative-cash-flow/calculate/page.tsx
+++ src/app/dashboard/(routes)/tools/creative-cash-flow/calculate/page.tsx
@@ -1,14 +1,12 @@
 import { captureException } from '@sentry/nextjs';
 import type { Metadata } from 'next';
 
-import { createSupabase } from '@/lib/supabase/server/createSupabase';
 import { getUser } from '@/lib/supabase/server/getUser';
 import { PageError } from '@/components/page-error';
 import { Separator } from '@/components/ui/separator';
 import { Container } from './components/container';
-
-import { Transaction } from '@/lib/plaid/types/transactions';
 import { Results } from './components/results';
+import { getData } from './getData';
 
 export const metadata: Metadata = {
   title: 'Creative Cash Flow',
@@ -16,36 +14,9 @@ export const metadata: Metadata = {
 
 export default async function CreativeCashFlowCalculatePage() {
   const user = (await getUser()) as User;
-  // Get all of the users transactions data split into business and personal
-  const supabase = createSupabase();
-  const { error: transactionsError, data: transactionsData } = await supabase.rpc(
-    'get_transactions_by_user_id',
-    {
-      arg_user_id: user.id,
-    }
-  );
+  const { error, data } = await getData(user.id);
 
-  if (transactionsError || !transactionsData) {
-    const error = transactionsError || new Error('No transactionsData returned');
-    console.error(error);
-    captureException(error);
-    return <PageError />;
-  }
-
-  const transactions = transactionsData as {
-    business: Transaction[];
-    personal: Transaction[];
-  };
-
-  // Get the users year to date collections value to add to the Year to Date results
-  const { error: collectionsError, data: collectionsData } = await supabase
-    .from('personal_finance')
-    .select('ytd_collections')
-    .eq('user_id', user.id)
-    .single();
-
-  if (collectionsError || !collectionsData) {
-    const error = transactionsError || new Error('No collectionsData returned');
+  if (error) {
     console.error(error);
     captureException(error);
     return <PageError />;
@@ -62,8 +33,8 @@ export default async function CreativeCashFlowCalculatePage() {
       <Separator className="mt-6" />
       <Container
         user_id={user.id}
-        transactions={transactions}
-        ytd_collections={collectionsData.ytd_collections}
+        transactions={data.transactions}
+        ytd_collections={data.ytd_collections}
       />
       <Results />
     </div>
