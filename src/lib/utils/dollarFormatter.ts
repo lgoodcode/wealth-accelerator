ce8c364d09cefec7ff5b5b3c4e133d9b1acb10b9
Lawrence Good <lawrence.good55@gmail.com>
2023-07-20 10:22:27 -0700

    Update dollarFomatter with options parameter

--- src/lib/utils/dollarFormatter.ts
+++ src/lib/utils/dollarFormatter.ts
@@ -1,3 +1,7 @@
-export const dollarFormatter = (value: number) => {
-  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value);
+export const dollarFormatter = (value: number, options?: Intl.NumberFormatOptions) => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    ...options,
+  }).format(value);
 };
