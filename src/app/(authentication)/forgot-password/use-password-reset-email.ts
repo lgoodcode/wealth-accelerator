bad8f7228b1c4b11cc83e8ca5bc22928591c2e6e
Lawrence Good <lawrence.good55@gmail.com>
2023-07-26 11:47:25 -0700

    Test

--- src/app/(authentication)/forgot-password/use-password-reset-email.ts
+++ src/app/(authentication)/forgot-password/use-password-reset-email.ts
@@ -3,8 +3,7 @@ import { supabase } from '@/lib/supabase/client';
 export const usePasswordResetEmail = () => {
   return async (email: string) => {
     const { error } = await supabase.auth.resetPasswordForEmail(email, {
-      // redirectTo: `${location.origin}/api/auth/callback?redirect_to=/reset-password`,
-      redirectTo: `https://staging.app.chirowealth.com/api/auth/callback?redirect_to=/reset-password`,
+      redirectTo: `${location.origin}/api/auth/callback?redirect_to=/reset-password`,
     });
 
     if (error) {
