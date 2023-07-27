aad906494a6107ef5416c87a867b6dde72417389
Lawrence Good <lawrence.good55@gmail.com>
2023-07-27 12:03:09 -0700

    dia

--- src/app/dashboard/(routes)/admin/insurance-policies/components/table/columns.tsx
+++ src/app/dashboard/(routes)/admin/insurance-policies/components/table/columns.tsx
@@ -17,68 +17,66 @@ import { RowActions } from './row-actions';
  */
 
 export const columns: ColumnDef<User>[] = [
-  {
-    accessorKey: 'name',
-    enableHiding: false,
-    enableGlobalFilter: true,
-    header: ({ column }) => <ColumnHeader column={column} title="Name" />,
-    cell: ({ row }) => {
-      return (
-        <div className="flex space-x-2">
-          <span className="max-w-[500px] truncate font-medium">{row.getValue<string>('name')}</span>
-        </div>
-      );
-    },
-    filterFn: (row, id, value) => {
-      const valueToCompare = row.getValue<string>(id).toLowerCase();
-      return valueToCompare.includes(value.toLowerCase());
-    },
-  },
-  {
-    accessorKey: 'email',
-    enableHiding: false,
-    enableGlobalFilter: true,
-    header: ({ column }) => <ColumnHeader column={column} title="Email" />,
-    cell: ({ row }) => {
-      return (
-        <div className="flex space-x-2">
-          <span className="max-w-[500px] truncate font-medium">
-            {row.getValue<string>('email')}
-          </span>
-        </div>
-      );
-    },
-    filterFn: (row, id, value) => {
-      const valueToCompare = row.getValue<string>(id).toLowerCase();
-      return valueToCompare.includes(value.toLowerCase());
-    },
-  },
-  {
-    accessorKey: 'role',
-    header: ({ column }) => <ColumnHeader column={column} title="Role" />,
-    cell: ({ row }) => {
-      const category = roleOptions.find((option) => option.value === row.getValue<Role>('role'));
-
-      if (!category) {
-        return null;
-      }
-
-      return (
-        <div className="flex items-center">
-          <span>{category.label}</span>
-        </div>
-      );
-    },
-    filterFn: (row, id, value) => {
-      return value.includes(row.getValue(id));
-    },
-  },
-  {
-    id: 'actions',
-    cell: ({ row }) => (
-      <div className="flex justify-end">
-        <RowActions row={row} />
-      </div>
-    ),
-  },
+  // {
+  //   accessorKey: 'name',
+  //   enableHiding: false,
+  //   enableGlobalFilter: true,
+  //   header: ({ column }) => <ColumnHeader column={column} title="Name" />,
+  //   cell: ({ row }) => {
+  //     return (
+  //       <div className="flex space-x-2">
+  //         <span className="max-w-[500px] truncate font-medium">{row.getValue<string>('name')}</span>
+  //       </div>
+  //     );
+  //   },
+  //   filterFn: (row, id, value) => {
+  //     const valueToCompare = row.getValue<string>(id).toLowerCase();
+  //     return valueToCompare.includes(value.toLowerCase());
+  //   },
+  // },
+  // {
+  //   accessorKey: 'email',
+  //   enableHiding: false,
+  //   enableGlobalFilter: true,
+  //   header: ({ column }) => <ColumnHeader column={column} title="Email" />,
+  //   cell: ({ row }) => {
+  //     return (
+  //       <div className="flex space-x-2">
+  //         <span className="max-w-[500px] truncate font-medium">
+  //           {row.getValue<string>('email')}
+  //         </span>
+  //       </div>
+  //     );
+  //   },
+  //   filterFn: (row, id, value) => {
+  //     const valueToCompare = row.getValue<string>(id).toLowerCase();
+  //     return valueToCompare.includes(value.toLowerCase());
+  //   },
+  // },
+  // {
+  //   accessorKey: 'role',
+  //   header: ({ column }) => <ColumnHeader column={column} title="Role" />,
+  //   cell: ({ row }) => {
+  //     const category = roleOptions.find((option) => option.value === row.getValue<Role>('role'));
+  //     if (!category) {
+  //       return null;
+  //     }
+  //     return (
+  //       <div className="flex items-center">
+  //         <span>{category.label}</span>
+  //       </div>
+  //     );
+  //   },
+  //   filterFn: (row, id, value) => {
+  //     return value.includes(row.getValue(id));
+  //   },
+  // },
+  // {
+  //   id: 'actions',
+  //   cell: ({ row }) => (
+  //     <div className="flex justify-end">
+  //       <RowActions row={row} />
+  //     </div>
+  //   ),
+  // },
 ];
