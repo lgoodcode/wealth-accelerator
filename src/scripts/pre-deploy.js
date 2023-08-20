0a2bbe966913791ee7569f898061c417dd3bf0be
Lawrence Good <lawrence.good55@gmail.com>
2023-08-20 13:30:42 -0700

    remove

--- src/scripts/pre-deploy.js
+++ src/scripts/pre-deploy.js
@@ -1,4 +1,25 @@
-import { requiredEnvList } from '@/config/required-env-list';
+export const requiredEnvList = [
+  'NEXT_PUBLIC_SUPABASE_URL',
+  'NEXT_PUBLIC_SUPABASE_ANON_KEY',
+  'SUPABASE_SERVICE_ROLE_KEY',
+  'SUPABASE_PROJECT_ID',
+  'SENTRY_PROJECT',
+  'SENTRY_ORG',
+  'NEXT_PUBLIC_SENTRY_DSN',
+  'SENTRY_AUTH_TOKEN',
+  'PLAID_ENV',
+  'PLAID_CLIENT_NAME',
+  'PLAID_CLIENT_ID',
+  'PLAID_URL',
+  'PLAID_SECRET',
+  'PLAID_VERSION',
+  'PLAID_REDIRECT_URI',
+  'PLAID_WEBHOOK_URI',
+  'SMTP2GO_API_KEY',
+  'SMTP2GO_API_URL',
+  'EMAIL_SENDER',
+  'CCF_RECORD_TEMPLATE',
+];
 
 // Skip checking environment variables if in local development or running on CI
 if (process.env.NODE_ENV && process.env.NODE_ENV !== 'development' && !process.env.CircleCI) {
