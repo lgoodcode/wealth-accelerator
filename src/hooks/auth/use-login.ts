ed5af64dac88eb9c6425757f63d16ad5ee81f95d
Lawrence Good <lawrence.good55@gmail.com>
2023-08-19 17:17:46 -0700

    Update useLogin hook for additional params besides redirect

--- src/hooks/auth/use-login.ts
+++ src/hooks/auth/use-login.ts
@@ -1,11 +1,18 @@
 import { useRouter, useSearchParams } from 'next/navigation';
 
 import { supabase } from '@/lib/supabase/client';
+import { formatPath } from '@/lib/utils/formatPath';
 
 export const useLogin = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const redirectTo = searchParams.get('redirect_to');
+  // Get all search parameters
+  const allSearchParams = Object.fromEntries(searchParams.entries());
+  // Remove redirect_to if it exists
+  const redirectTo: `/${string}` = formatPath(allSearchParams.redirect_to);
+  delete allSearchParams.redirect_to;
+
+  const finalRedirectTo = `${redirectTo}?${new URLSearchParams(allSearchParams).toString()}`;
 
   return async (data: { email: string; password: string }) => {
     const { error } = await supabase.auth.signInWithPassword({
@@ -17,6 +24,6 @@ export const useLogin = () => {
       throw error;
     }
 
-    router.push(redirectTo || '/dashboard/home');
+    router.push(finalRedirectTo || '/dashboard/home');
   };
 };
