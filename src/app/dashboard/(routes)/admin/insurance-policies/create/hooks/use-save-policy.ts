e23cb87b1eb08ab3f4efdf61811a27d1f44df717
Lawrence Good <lawrence.good55@gmail.com>
2023-08-20 21:53:50 -0700

    disable unused insurance policy sql stuff

--- src/app/dashboard/(routes)/admin/insurance-policies/create/hooks/use-save-policy.ts
+++ src/app/dashboard/(routes)/admin/insurance-policies/create/hooks/use-save-policy.ts
@@ -14,20 +14,20 @@ export const useSavePolicy = () => {
       throw new Error('No insurance company was selected');
     }
 
-    const { error } = await supabase.rpc('create_insurance_policy', {
-      p_user_id: user_id,
-      p_company_id: companyId,
-      p_name: policy_name,
-      p_policy_rows: rows,
-    });
+    // const { error } = await supabase.rpc('create_insurance_policy', {
+    //   p_user_id: user_id,
+    //   p_company_id: companyId,
+    //   p_name: policy_name,
+    //   p_policy_rows: rows,
+    // });
 
-    if (error) {
-      throw error;
-    }
+    // if (error) {
+    //   throw error;
+    // }
 
-    setRows([]);
-    setCompanyId(-1);
-    // Refresh the page to get the new policy and retain the state
-    router.refresh();
+    // setRows([]);
+    // setCompanyId(-1);
+    // // Refresh the page to get the new policy and retain the state
+    // router.refresh();
   };
 };
