67dfdaef5afd770cf88861bdff39a5af341d483f
Lawrence Good <lawrence.good55@gmail.com>
2023-08-22 11:32:21 -0700

    Fix getItemFromId for webhook admin instance

--- src/lib/plaid/getItemFromItemId.ts
+++ src/lib/plaid/getItemFromItemId.ts
@@ -6,7 +6,7 @@ import type { Institution } from './types/institutions';
  * Retrieves the institution item from the database using the item_id
  */
 export const getItemFromItemId = async (item_id: string, admin?: boolean) => {
-  const supabase = admin ? createSupabase() : supabaseAdmin;
+  const supabase = admin ? supabaseAdmin : createSupabase();
   const { error, data } = await supabase.from('plaid').select('*').eq('item_id', item_id).single();
 
   if (error || !data) {
