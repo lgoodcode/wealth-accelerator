51c33eaf48fab80d8f0a1a4e51cb6fb9d784e69d
Lawrence Good <lawrence.good55@gmail.com>
2023-08-21 19:01:26 -0700

    Update seed data with CCF

---
+++ supabase/insurance_companies.sql
@@ -0,0 +1,7 @@
+-- Use this data once insurance companies are implemented, if ever
+
+-- INSERT INTO public.insurance_companies(name)
+-- VALUES
+--   ('Mass Mutual'),
+--   ('Lafayette Life'),
+--   ('AUL Inforce');
