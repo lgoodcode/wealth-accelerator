6e495cb567033195aae9abe2fc653e231526f10e
Lawrence Good <lawrence.good55@gmail.com>
2023-08-24 08:37:09 -0700

    Revert "tset"
    This reverts commit 09eee0fa44c326640c4c612b0fa8f95629e45f7a.

--- src/hooks/use-shims.tsx
+++ src/hooks/use-shims.tsx
@@ -6,7 +6,6 @@ import { structuredCloneShim } from '@/lib/utils/structured-clone-shim';
 
 export function useShims() {
   useEffect(() => {
-    console.log('shimmy');
     structuredCloneShim();
   }, []);
 }
