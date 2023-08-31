d53a2d34f6cd8cdbc661f70d8f267a988efd927a
Lawrence Good <lawrence.good55@gmail.com>
2023-08-31 13:17:41 -0700

    log

--- next.config.js
+++ next.config.js
@@ -67,7 +67,7 @@ module.exports = withBundleAnalyzer(
   withSentryConfig(
     nextConfig,
     {
-      silent: true,
+      silent: false,
       authToken: process.env.SENTRY_AUTH_TOKEN,
     },
     {
