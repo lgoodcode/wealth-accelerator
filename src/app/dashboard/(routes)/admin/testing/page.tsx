cd3fc82247c7caae25ba90dbee6fce368de3c6ae
Lawrence Good <lawrence.good55@gmail.com>
2023-09-08 13:23:22 -0700

    Remove unused item and check

--- src/app/dashboard/(routes)/admin/testing/page.tsx
+++ src/app/dashboard/(routes)/admin/testing/page.tsx
@@ -1,8 +1,5 @@
-import { redirect } from 'next/navigation';
 import type { Metadata } from 'next';
 
-import { getUser } from '@/lib/supabase/server/get-user';
-import { isAdmin } from '@/lib/utils/is-admin';
 import { Separator } from '@/components/ui/separator';
 import { TestingContent } from './testing-content';
 
@@ -11,12 +8,6 @@ export const metadata: Metadata = {
 };
 
 export default async function TestPage() {
-  const user = (await getUser()) as User;
-
-  if (!isAdmin(user)) {
-    redirect('/dashboard');
-  }
-
   return (
     <div className="p-8">
       <div className="space-y-1">
