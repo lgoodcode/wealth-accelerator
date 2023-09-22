ca8190cff5822275536e5cdb46cf06e689da5685
Lawrence Good <lawrence.good55@gmail.com>
2023-09-22 15:38:24 -0700

    update admin filtering column

--- src/app/dashboard/(routes)/admin/transaction-filtering/components/table/columns.tsx
+++ src/app/dashboard/(routes)/admin/transaction-filtering/components/table/columns.tsx
@@ -7,47 +7,9 @@ import { ColumnHeader } from './column-header';
 import { RowActions } from './row-actions';
 import { Category, type Filter } from '@/lib/plaid/types/transactions';
 
-/**
- * NOTES
- *
- * `row.getValue<string>('name')`, when retrieving the value of a column, specify the type of the value
- * to ensure that the value is of the correct type.
- *
- * The filterFn `value` is the value set from the table.getColumn('date')?.setFilterValue() call.
- */
-
 export const columns: ColumnDef<Filter>[] = [
-  // {
-  //   id: 'select',
-  //   header: ({ table }) => (
-  //     <Checkbox
-  //       checked={table.getIsAllPageRowsSelected()}
-  //       onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-  //       aria-label="Select all"
-  //       className="translate-y-[2px]"
-  //     />
-  //   ),
-  //   cell: ({ row }) => (
-  //     <Checkbox
-  //       checked={row.getIsSelected()}
-  //       onCheckedChange={(value) => row.toggleSelected(!!value)}
-  //       aria-label="Select row"
-  //       className="translate-y-[2px]"
-  //     />
-  //   ),
-  //   enableSorting: false,
-  //   enableHiding: false,
-  // },
-  // {
-  //   accessorKey: 'id',
-  //   header: ({ column }) => <ColumnHeader column={column} title="Task" />,
-  //   cell: ({ row }) => <div className="w-[80px]">{row.getValue('id')}</div>,
-  //   enableSorting: false,
-  //   enableHiding: false,
-  // },
   {
     accessorKey: 'filter',
-    enableHiding: false,
     header: ({ column }) => <ColumnHeader column={column} title="Filter" />,
     cell: ({ row }) => {
       return (
