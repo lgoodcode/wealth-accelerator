1c63a77ce4b744bd378746c96d754b985250b848
Lawrence Good <lawrence.good55@gmail.com>
2023-10-01 19:08:34 -0700

    header bg

--- src/components/public/header.tsx
+++ src/components/public/header.tsx
@@ -6,11 +6,11 @@ import { Button } from '@/components/ui/button';
 export function Header() {
   return (
     <div className="sticky z-50 top-0 w-full flex justify-center">
-      <nav className="absolute w-full bg-white/80 bd-sat-blur">
+      <nav className="absolute w-full bg-white/90 bd-frost">
         <div className="container relative flex max-w-6xl mx-auto h-16 justify-between items-center">
-          <Link href="https://www.chirowealth.com" target="_blank" rel="noreferrer">
+          <a href="https://www.chirowealth.com" target="_blank" rel="noreferrer">
             <Image src="/img/logo-318x85.png" alt="logo" priority width={160} height={43} />
-          </Link>
+          </a>
           <ul className="flex gap-3">
             {/* <li>
               <a
