55072c002da1610d9ce40f133b036f45a9bdbbee
Lawrence Good <lawrence.good55@gmail.com>
2023-10-01 19:01:57 -0700

    login and home image linking

--- src/components/public/header.tsx
+++ src/components/public/header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Image from 'next/image';
 
 import { Button } from '@/components/ui/button';
@@ -7,9 +8,9 @@ export function Header() {
     <div className="sticky z-50 top-0 w-full flex justify-center">
       <nav className="absolute w-full bg-white/80 bd-sat-blur">
         <div className="container relative flex max-w-6xl mx-auto h-16 justify-between items-center">
-          <a href="https://www.chirowealth.com" target="_blank" rel="noreferrer">
+          <Link href="https://www.chirowealth.com" target="_blank" rel="noreferrer">
             <Image src="/img/logo-318x85.png" alt="logo" priority width={160} height={43} />
-          </a>
+          </Link>
           <ul className="flex gap-3">
             {/* <li>
               <a
@@ -35,7 +36,7 @@ export function Header() {
             </li>
           </ul>
           <Button>
-            <a href="/login">Sign In</a>
+            <Link href="/login">Sign In</Link>
           </Button>
         </div>
       </nav>
