7deb0c0d9063c0f00696062341fb731aea97b506
Lawrence Good <lawrence.good55@gmail.com>
2023-10-01 21:49:14 -0700

    footer

--- src/components/public/footer.tsx
+++ src/components/public/footer.tsx
@@ -1,51 +1,47 @@
+import Link from 'next/link';
+
+import { faker } from '@faker-js/faker';
+
 export function Footer() {
   return (
-    <footer className="bg-gray-800 p-6">
-      <div className="container mx-auto">
+    <footer className="bg-gray-800 pt-8 pb-4">
+      <div className="container mx-auto mb-8">
         <div className="flex flex-wrap justify-between">
           <div className="w-full md:w-1/3 mb-6 md:mb-0">
             <h4 className="text-white mb-2">About Us</h4>
-            <p className="text-gray-400">Short description about the company.</p>
+            <p className="text-gray-400">{faker.lorem.sentences(6)}</p>
           </div>
+
           <div className="w-full md:w-1/3 mb-6 md:mb-0">
             <h4 className="text-white mb-2">Quick Links</h4>
-            <ul className="text-gray-400">
-              <li>
-                <a href="#" className="hover:text-white">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-white">
-                  Services
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-white">
-                  Contact
-                </a>
-              </li>
-            </ul>
+            <div className="flex flex-col gap-1 w-fit">
+              <Link href="#" className="text-gray-400 hover:text-gray-300 capitalize">
+                Sign In
+              </Link>
+              <Link href="#" className="text-gray-400 hover:text-gray-300 capitalize">
+                Contact Us
+              </Link>
+            </div>
           </div>
+
           <div className="w-full md:w-1/3 mb-6 md:mb-0">
-            <h4 className="text-white mb-2">Newsletter</h4>
-          </div>
-        </div>
-        <div className="flex items-center justify-between mt-4">
-          <div className="flex items-center space-x-3">
-            <a href="#" className="hover:text-blue-400">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="#" className="hover:text-blue-400">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="#" className="hover:text-blue-400">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
+            <h4 className="text-white mb-2">Legal</h4>
+            <div className="flex flex-col gap-1 w-fit">
+              <Link href="/terms" className="text-gray-400 hover:text-gray-300 capitalize">
+                Terms of Service
+              </Link>
+              <Link href="/privacy" className="text-gray-400 hover:text-gray-300 capitalize">
+                Privacy Policy
+              </Link>
+              <Link href="/cookies" className="text-gray-400 hover:text-gray-300 capitalize">
+                Cookie Policy
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-      <div className="mt-8 text-center">
+
+      <div className="text-center">
         <p className="text-gray-400">&copy; 2023 Your Company. All rights reserved.</p>
       </div>
     </footer>
