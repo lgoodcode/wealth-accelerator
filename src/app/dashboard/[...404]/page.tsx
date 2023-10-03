80a78d06579dc12388efc8f8af0161ddf8a2af52
Lawrence Good <lawrence.good55@gmail.com>
2023-10-03 15:11:00 -0700

    dashboard 404 page

---
+++ src/app/dashboard/[...404]/page.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+export default function DashboardNotFoundPage() {
+  return (
+    <div className="container font-sans mt-32 grid grid-cols-2 gap-8 lg:gap-0">
+      <div className="col-span-2 lg:col-span-1 my-auto space-y-6">
+        <h1 className="text-4xl font-semibold leading-snug">
+          Oops, the page you&apos;re looking for wasn&apos;t found.
+        </h1>
+      </div>
+
+      <div className="col-span-2 lg:col-span-1 relative">
+        <Image
+          src="/img/404-robot.png"
+          alt="404 illustration - robot"
+          priority
+          width={2500}
+          height={1778}
+        />
+      </div>
+    </div>
+  );
+}
