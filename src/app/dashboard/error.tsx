a206a9b96cf3548781024af369775936fb56fbae
Lawrence Good <lawrence.good55@gmail.com>
2023-10-03 15:30:55 -0700

    update 500 page for dashboard

--- src/app/dashboard/error.tsx
+++ src/app/dashboard/error.tsx
@@ -1,12 +1,13 @@
 'use client';
 
+import Image from 'next/image';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { captureException } from '@sentry/nextjs';
 
 import { Button } from '@/components/ui/button';
 
-export default function Error({ error, reset }: any) {
+export default function InternalErrorPage({ error, reset }: any) {
   const router = useRouter();
   const [isRetrying, setIsRetrying] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
@@ -22,15 +23,32 @@ export default function Error({ error, reset }: any) {
   captureException(error);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-semibold">Oops, an error occured.</h1>
-      <div className="mt-6 flex gap-4">
-        <Button loading={isRetrying} onClick={handleReset}>
-          Reload
-        </Button>
-        <Button loading={isLoading} onClick={handleGoHome}>
-          Go home
-        </Button>
+    <div className="container font-sans mt-32 grid grid-cols-2 gap-8 lg:gap-0">
+      <div className="col-span-2 lg:col-span-1 my-auto space-y-6">
+        <div>
+          <h1 className="text-4xl font-semibold leading-snug">Something went wrong</h1>
+          <p className="text-lg text-muted-foreground">
+            Try reloading the content or navigate to a different page or refresh the page.
+          </p>
+        </div>
+        <div className="flex gap-4">
+          <Button loading={isRetrying} onClick={handleReset}>
+            Reload
+          </Button>
+          <Button loading={isLoading} onClick={handleGoHome}>
+            Go home
+          </Button>
+        </div>
+      </div>
+
+      <div className="col-span-2 lg:col-span-1 relative">
+        <Image
+          src="/img/500-dog.png"
+          alt="500 error dog with computer - Image by storyset on Freepik"
+          width={668}
+          height={668}
+          priority
+        />
       </div>
     </div>
   );
