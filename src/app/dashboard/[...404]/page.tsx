f5b67fc2a2c83af95730d1126c3656d3f525aae3
Lawrence Good <lawrence.good55@gmail.com>
2023-10-03 17:48:55 -0700

    dashboard 404 page

--- src/app/dashboard/[...404]/page.tsx
+++ src/app/dashboard/[...404]/page.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image';
 
 export default function DashboardNotFoundPage() {
   return (
-    <div className="container font-sans mt-32 grid grid-cols-2 gap-8 lg:gap-0">
-      <div className="col-span-2 lg:col-span-1 my-auto space-y-6">
+    <div className="container h-full mt-32 grid grid-cols-2 gap-8">
+      <div className="container col-span-2 lg:col-span-1 my-auto space-y-6">
         <h1 className="text-4xl font-semibold leading-snug">
           Oops, the page you&apos;re looking for wasn&apos;t found.
         </h1>
       </div>
 
-      <div className="col-span-2 lg:col-span-1 relative">
+      <div className="col-span-2 lg:col-span-1 flex items-center justify-center">
         <Image
           src="/img/404-robot.png"
           alt="404 illustration - robot"
