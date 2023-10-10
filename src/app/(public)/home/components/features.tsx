3baa51ca897ebade7f2e03cdd962e3eb1f0c59a1
Lawrence Good <lawrence.good55@gmail.com>
2023-10-09 22:13:27 -0700

    update icons

--- src/app/(public)/home/components/features.tsx
+++ src/app/(public)/home/components/features.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
-import { Box } from 'lucide-react';
+import { DollarSign, Landmark, Snowflake, type LucideIcon } from 'lucide-react';
 
 import { cn } from '@/lib/utils/cn';
 
 interface FeatureItemProps {
   title: string;
   description: string;
+  Icon: LucideIcon;
   image: {
     src: string;
     alt: string;
@@ -16,7 +17,7 @@ interface FeatureItemProps {
   };
 }
 
-const FeatureItem = ({ title, description, image }: FeatureItemProps) => {
+const FeatureItem = ({ title, description, Icon, image }: FeatureItemProps) => {
   return (
     <div className="col-span-12 grid grid-cols-12 gap-16 lg:gap-8">
       <div
@@ -26,7 +27,7 @@ const FeatureItem = ({ title, description, image }: FeatureItemProps) => {
       >
         <div className="space-y-6 text-center">
           <div className="p-4 mx-auto w-fit bg-gradient-to-br from-cyan-600 to-cyan-900 text-white rounded-full">
-            <Box size={48} className="mx-auto" />
+            <Icon size={48} className="mx-auto" />
           </div>
           <h5 className="text-4xl font-semibold capitalize">{title}</h5>
           <p className="text-2xl max-w-md mx-auto">{description}</p>
@@ -59,6 +60,7 @@ export function Features({ className }: { className?: string }) {
         <FeatureItem
           title="Connect your bank accounts"
           description="Connect your bank accounts to Wealth Accelerator securely and with ease. Plaid supports over 10,000 financial institutions."
+          Icon={Landmark}
           image={{
             src: '/img/mocks/banking.webp',
             alt: 'Mock of banking feature in Wealth Accelerator',
@@ -70,6 +72,7 @@ export function Features({ className }: { className?: string }) {
         <FeatureItem
           title="Creative Cash Flow"
           description="After linking your bank accounts, Wealth Accelerator will automatically categorize your transactions, providing a clear visualization of where your money is being spent."
+          Icon={DollarSign}
           image={{
             src: '/img/mocks/creative-cash-flow.webp',
             alt: 'Mock of Creative Cash Flow feature in Wealth Accelerator',
@@ -81,6 +84,7 @@ export function Features({ className }: { className?: string }) {
         <FeatureItem
           title="Debt Snowball"
           description="Wealth Accelerator will present various strategies and compare them to help you find the most efficient way to pay off your debts quickly."
+          Icon={Snowflake}
           image={{
             src: '/img/mocks/debt-snowball.webp',
             alt: 'Mock of Debt Snowball feature in Wealth Accelerator',
