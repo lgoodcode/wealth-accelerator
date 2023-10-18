d4a4780f38c479dfc351ac89b77a01d5f6c3d271
Lawrence Good <lawrence.good55@gmail.com>
2023-10-17 22:37:52 -0700

    rearrange testimonials

--- src/app/(public)/home/components/testimonials.tsx
+++ src/app/(public)/home/components/testimonials.tsx
@@ -24,12 +24,12 @@ export const testimonials: Testimonial[] = [
     \nThank-you`,
   },
   {
-    name: 'Dr. Roger Smith',
-    personalImage: '/img/testimonials/roger-smith.jpg',
-    companyImage: '/img/testimonials/roger-smith-company.png',
-    companyUrl: 'https://rogerdsmithdc.com/',
+    name: 'Dr. Steve Byrne',
+    personalImage: '/img/testimonials/steve-byrne.jpg',
+    companyImage: '/img/testimonials/steve-byrne-company.png',
+    companyUrl: 'https://www.byrnechiropractic.com/dr-stephen-byrne/',
     message:
-      "I was told back in Chiropractic school that you could make a good living being a Chiropractor but would not become wealthy. To do that you had to take the money you make and invest it wisely to become wealthy. Nice advice but what to do with it. What's the rest of the story. How do I invest, where do I invest and how do I learn what wise investing is? I made a good living over the years as a Chiropractor but didn't know how or where to invest so that I wouldn't lose the money I've worked so many years to save. So, I did nothing. I trudged away in my practice putting 10% of my take home pay aside in a saving account (because thats all the advice I could find). There it waited for me to discover how to invest. But that never came. Fast forward 15+ years and I'm cleaning out the garage, going through boxes and come across information about ChiroWealth Learning Systems.  I have that 10% I've been saving for many years now, not knowing what to do with it and I wonder if that guys still in business. So I look him up online and low and behold he's still there. He's got a website to schedule a meeting so I think I'll take a closer look. BEST GARAGE FIND EVER! I meet with Bruce and his program makes sense. It's just what I've been looking for. A complete roadmap program to set up to capture my profits and begin saving in secure, safe investments. Safe, secure investments that aren't get rich quick schemes. Now I wish I had made the call 15+ years ago. I could have already had years of return on my money instead of giving it away to Uncle Sam and the Banking system. The ChiroWealth system isn't just about the investing. That's the basis but really it is about the mindset you need to have in practice to truly succeed. I know Chiropractic but Bruce knows investing. He has filled the missing link I needed to connect my practice with my future. He has shown me how to focus myself towards what I want, where I want to go and what I need to do to get there. I couldn't be happier with the results I am already achieving. Within weeks of beginning to look into just some of the directions Bruce has guided us towards we have developed certainty, purpose and confidence in the fact that we are beginning an entirely different chapter in our Chiropractic, financial and personal life. I can't change the past or the hesitation I had towards this program but hopefully this may help someone considering this program to take the best step towards their future I believe they could make.",
+      "Bruce has been a god sent to me since I signed up with him only 3 months ago. Before signing up with Chirowealth, I saved a total of $24,000 in 15 years in practice. That's $1600 per year. Since joining Chirowealth, in 3 months I have saved over $14,000 and on my way to save $65,000 this year. I now have a clear financial plan towards my future for myself and my family. Thank-you Bruce for getting me on the right path.",
   },
   {
     name: 'Dr. Zach Sedivy, Italy',
@@ -48,20 +48,20 @@ export const testimonials: Testimonial[] = [
       'I have been a chiropractor for 34 years.  We have lived a great life, had the American dream. house, cars, vacations and more, but after 34 years I had no savings and no plan for the future. We started with Bruce at Chirowealth Learning Systems in November of 2015. In 8 months he has shown us how to save more than $10,000 a month and we are rapidly preparing for the future. His program has helped us to focus more on our business and saving for our future. Our practice has increased and our collections have grown. We are excited that we are now able to plan to add an associate to our practice. All of this without having to change our lifestyle. I think you are a wonderful and great coach for us.',
   },
   {
-    name: 'Dr. Steve Byrne',
-    personalImage: '/img/testimonials/steve-byrne.jpg',
-    companyImage: '/img/testimonials/steve-byrne-company.png',
-    companyUrl: 'https://www.byrnechiropractic.com/dr-stephen-byrne/',
-    message:
-      "Bruce has been a god sent to me since I signed up with him only 3 months ago. Before signing up with Chirowealth, I saved a total of $24,000 in 15 years in practice. That's $1600 per year. Since joining Chirowealth, in 3 months I have saved over $14,000 and on my way to save $65,000 this year. I now have a clear financial plan towards my future for myself and my family. Thank-you Bruce for getting me on the right path.",
-  },
-  {
     name: 'Dr. David Schwartz',
     personalImage: '/img/testimonials/david-schwartz.jpg',
     companyImage: '/img/testimonials/david-schwartz-company.jpg',
     companyUrl: 'https://triadhealthcenter.com/dr-dave-schwartz/',
     message:
       'What I love about the strategy you put in place for us is that I know we are safely investing for the future while continuing to push ourselves. You have provided me with a tool that keeps my mindset stay focused on keeping the pedal down and not letting up.',
+  },
+  {
+    name: 'Dr. Roger Smith',
+    personalImage: '/img/testimonials/roger-smith.jpg',
+    companyImage: '/img/testimonials/roger-smith-company.png',
+    companyUrl: 'https://rogerdsmithdc.com/',
+    message:
+      "I was told back in Chiropractic school that you could make a good living being a Chiropractor but would not become wealthy. To do that you had to take the money you make and invest it wisely to become wealthy. Nice advice but what to do with it. What's the rest of the story. How do I invest, where do I invest and how do I learn what wise investing is? I made a good living over the years as a Chiropractor but didn't know how or where to invest so that I wouldn't lose the money I've worked so many years to save. So, I did nothing. I trudged away in my practice putting 10% of my take home pay aside in a saving account (because thats all the advice I could find). There it waited for me to discover how to invest. But that never came. Fast forward 15+ years and I'm cleaning out the garage, going through boxes and come across information about ChiroWealth Learning Systems.  I have that 10% I've been saving for many years now, not knowing what to do with it and I wonder if that guys still in business. So I look him up online and low and behold he's still there. He's got a website to schedule a meeting so I think I'll take a closer look. BEST GARAGE FIND EVER! I meet with Bruce and his program makes sense. It's just what I've been looking for. A complete roadmap program to set up to capture my profits and begin saving in secure, safe investments. Safe, secure investments that aren't get rich quick schemes. Now I wish I had made the call 15+ years ago. I could have already had years of return on my money instead of giving it away to Uncle Sam and the Banking system. The ChiroWealth system isn't just about the investing. That's the basis but really it is about the mindset you need to have in practice to truly succeed. I know Chiropractic but Bruce knows investing. He has filled the missing link I needed to connect my practice with my future. He has shown me how to focus myself towards what I want, where I want to go and what I need to do to get there. I couldn't be happier with the results I am already achieving. Within weeks of beginning to look into just some of the directions Bruce has guided us towards we have developed certainty, purpose and confidence in the fact that we are beginning an entirely different chapter in our Chiropractic, financial and personal life. I can't change the past or the hesitation I had towards this program but hopefully this may help someone considering this program to take the best step towards their future I believe they could make.",
   },
 ];
 
