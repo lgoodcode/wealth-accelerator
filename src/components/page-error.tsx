a0bf0b38e2b7d7fab6b72507d685916aad471bf5
Lawrence Good <lawrence.good55@gmail.com>
2023-10-17 22:17:09 -0700

    Update page errors

--- src/components/page-error.tsx
+++ src/components/page-error.tsx
@@ -1,11 +1,7 @@
 'use client';
 
-import { captureException } from '@sentry/nextjs';
-
 import { DashboardError } from '@/components/dashboard-error';
 
-export function PageError({ error, reset }: any) {
-  captureException(error);
-
-  return <DashboardError reset={reset} />;
+export function PageError() {
+  return <DashboardError />;
 }
