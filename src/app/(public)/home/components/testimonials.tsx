c7959f4eced914f9263a065de40753c196b90c39
Lawrence Good <lawrence.good55@gmail.com>
2023-10-17 22:36:09 -0700

    Update testimonials

--- src/app/(public)/home/components/testimonials.tsx
+++ src/app/(public)/home/components/testimonials.tsx
@@ -4,7 +4,66 @@ import Image from 'next/image';
 import { motion } from 'framer-motion';
 
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
-import { testimonials, type Testimonial } from '../testimonials';
+
+export type Testimonial = {
+  name: string;
+  personalImage: `/img/testimonials/${string}`;
+  companyImage: `/img/testimonials/${string}`;
+  companyUrl: string;
+  message: string;
+};
+
+export const testimonials: Testimonial[] = [
+  {
+    name: 'Susan Mitchell',
+    personalImage: '/img/testimonials/susan-mitchell.jpg',
+    companyImage: '/img/testimonials/susan-mitchell-company.jpg',
+    companyUrl: 'https://www.mitchellfamilychiro.com/',
+    message: `Bruce and Win with Chirowealth Learning Systems helped me transform my Chiropractic practice and my coaching business in ways I could not imagine. My name is Dr. Susan Mitchell. I have been in practice for over 20 years and have been a Chiropractic coach for 8 years. I have been coaching with Bruce for the past 5 years and I have taken my practice from $1.5 to $2.5 million in collections.
+    I have also set myself up for a great retirement. Unfortunately, I got a late start in the game of saving. I have always been all about patient care and serving my community. I believed “if you take care of the patient, the money will take care of itself”. Well, the money did come, but I was not watching it nor saving it in an intentional way. Chirowealth Learning Systems taught me how to make my money work for me and for my future. I could not be more thankful for their guidance and knowledge. They set me up for such great success both professionally and personally.
+    \nThank-you`,
+  },
+  {
+    name: 'Dr. Roger Smith',
+    personalImage: '/img/testimonials/roger-smith.jpg',
+    companyImage: '/img/testimonials/roger-smith-company.png',
+    companyUrl: 'https://rogerdsmithdc.com/',
+    message:
+      "I was told back in Chiropractic school that you could make a good living being a Chiropractor but would not become wealthy. To do that you had to take the money you make and invest it wisely to become wealthy. Nice advice but what to do with it. What's the rest of the story. How do I invest, where do I invest and how do I learn what wise investing is? I made a good living over the years as a Chiropractor but didn't know how or where to invest so that I wouldn't lose the money I've worked so many years to save. So, I did nothing. I trudged away in my practice putting 10% of my take home pay aside in a saving account (because thats all the advice I could find). There it waited for me to discover how to invest. But that never came. Fast forward 15+ years and I'm cleaning out the garage, going through boxes and come across information about ChiroWealth Learning Systems.  I have that 10% I've been saving for many years now, not knowing what to do with it and I wonder if that guys still in business. So I look him up online and low and behold he's still there. He's got a website to schedule a meeting so I think I'll take a closer look. BEST GARAGE FIND EVER! I meet with Bruce and his program makes sense. It's just what I've been looking for. A complete roadmap program to set up to capture my profits and begin saving in secure, safe investments. Safe, secure investments that aren't get rich quick schemes. Now I wish I had made the call 15+ years ago. I could have already had years of return on my money instead of giving it away to Uncle Sam and the Banking system. The ChiroWealth system isn't just about the investing. That's the basis but really it is about the mindset you need to have in practice to truly succeed. I know Chiropractic but Bruce knows investing. He has filled the missing link I needed to connect my practice with my future. He has shown me how to focus myself towards what I want, where I want to go and what I need to do to get there. I couldn't be happier with the results I am already achieving. Within weeks of beginning to look into just some of the directions Bruce has guided us towards we have developed certainty, purpose and confidence in the fact that we are beginning an entirely different chapter in our Chiropractic, financial and personal life. I can't change the past or the hesitation I had towards this program but hopefully this may help someone considering this program to take the best step towards their future I believe they could make.",
+  },
+  {
+    name: 'Dr. Zach Sedivy, Italy',
+    personalImage: '/img/testimonials/zach-sedivy.jpg',
+    companyImage: '/img/testimonials/zach-sedivy-company.png',
+    companyUrl: 'https://uppercervicaltreviso.it/',
+    message:
+      'I have completely changed how I look at my money. I have realized that just like with health, wealth is not created by treating symptoms but by changing the fundamental beliefs in how to deal with finances.In my first year in practice I had saved about $8000 and I thought I was doing awesome! Since starting Chirowealth with Bruce, not only have I saved over $25,000, but my practice is finally starting to take off, too! And this is only after four months in the program!I truly appreciate how Bruce has gotten me on the right path and I am excited to grow even more in the next year and see myself surpassing all my goals and dreams!',
+  },
+  {
+    name: 'Dr. Marc Weiss',
+    personalImage: '/img/testimonials/marc-weiss.jpg',
+    companyImage: '/img/testimonials/marc-weiss-company.png',
+    companyUrl: 'https://www.chiropractortorrance.com/',
+    message:
+      'I have been a chiropractor for 34 years.  We have lived a great life, had the American dream. house, cars, vacations and more, but after 34 years I had no savings and no plan for the future. We started with Bruce at Chirowealth Learning Systems in November of 2015. In 8 months he has shown us how to save more than $10,000 a month and we are rapidly preparing for the future. His program has helped us to focus more on our business and saving for our future. Our practice has increased and our collections have grown. We are excited that we are now able to plan to add an associate to our practice. All of this without having to change our lifestyle. I think you are a wonderful and great coach for us.',
+  },
+  {
+    name: 'Dr. Steve Byrne',
+    personalImage: '/img/testimonials/steve-byrne.jpg',
+    companyImage: '/img/testimonials/steve-byrne-company.png',
+    companyUrl: 'https://www.byrnechiropractic.com/dr-stephen-byrne/',
+    message:
+      "Bruce has been a god sent to me since I signed up with him only 3 months ago. Before signing up with Chirowealth, I saved a total of $24,000 in 15 years in practice. That's $1600 per year. Since joining Chirowealth, in 3 months I have saved over $14,000 and on my way to save $65,000 this year. I now have a clear financial plan towards my future for myself and my family. Thank-you Bruce for getting me on the right path.",
+  },
+  {
+    name: 'Dr. David Schwartz',
+    personalImage: '/img/testimonials/david-schwartz.jpg',
+    companyImage: '/img/testimonials/david-schwartz-company.jpg',
+    companyUrl: 'https://triadhealthcenter.com/dr-dave-schwartz/',
+    message:
+      'What I love about the strategy you put in place for us is that I know we are safely investing for the future while continuing to push ourselves. You have provided me with a tool that keeps my mindset stay focused on keeping the pedal down and not letting up.',
+  },
+];
 
 const TesitmonialItem = ({ testimonial }: { testimonial: Testimonial }) => {
   return (
@@ -12,18 +71,21 @@ const TesitmonialItem = ({ testimonial }: { testimonial: Testimonial }) => {
       className="space-y-6 p-6 border shadow-md bg-white"
       variants={{ hidden: { opacity: 0, y: 60 }, visible: { opacity: 1, y: 0 } }}
     >
-      <div className="flex flex-row gap-4">
-        <Avatar>
-          <AvatarImage src={testimonial.personalImage} alt={`${testimonial.name}`} />
-        </Avatar>
-        <h3 className="text-lg font-bold">{testimonial.name}</h3>
-      </div>
-      <Image
-        src={testimonial.companyImage}
-        alt={`${testimonial.name}-practice`}
-        width={180}
-        height={47}
-      />
+      <a href={testimonial.companyUrl} className="space-y-4" referrerPolicy="origin" target="_self">
+        <div className="flex flex-row gap-4">
+          <Avatar>
+            <AvatarImage src={testimonial.personalImage} alt={`${testimonial.name}`} />
+          </Avatar>
+          <h3 className="text-lg font-bold">{testimonial.name}</h3>
+        </div>
+        <Image
+          src={testimonial.companyImage}
+          alt={`${testimonial.name}-practice`}
+          width={160}
+          height={47}
+        />
+      </a>
+
       <blockquote className="text-muted-foregroud italic line-clamp-[18]">
         <p>&quot;{testimonial.message}&quot;</p>
       </blockquote>
@@ -34,7 +96,7 @@ const TesitmonialItem = ({ testimonial }: { testimonial: Testimonial }) => {
 export function Testimonials({ className }: { className?: string }) {
   const col1 = testimonials.slice(0, 2);
   const col2 = testimonials.slice(2, 4);
-  const col3 = testimonials.slice(4, 5);
+  const col3 = testimonials.slice(4, 6);
 
   return (
     <motion.div
