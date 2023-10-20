05b65391d0972868a337dd878deb2423a0093eec
Lawrence Good <lawrence.good55@gmail.com>
2023-10-20 14:20:10 -0700

    banking grammar

--- src/app/dashboard/(routes)/finance/banking/help-content.tsx
+++ src/app/dashboard/(routes)/finance/banking/help-content.tsx
@@ -2,8 +2,8 @@ export const HelpContent = (
   <div className="space-y-4">
     <p>
       To get started, select the dropdown menu and add an institution. You will be redirected to
-      Plaid where you can link your bank account. Note: Plaid is a third-party service that allows
-      you to link your bank account to this application. You can read more about Plaid{' '}
+      Plaid where you can link your bank account. Note that Plaid is a third-party service that
+      allows you to link your bank account to this application. You can read more about Plaid{' '}
       <a
         href="https://plaid.com/legal/#end-user-privacy-policy"
         target="_blank"
@@ -12,20 +12,21 @@ export const HelpContent = (
       >
         here
       </a>
+      .
     </p>
     <p>
       After successfully connecting your institution, it will begin syncing information from your
-      bank with Plaid. This may take a few minutes. Once complete, you will be able to view your
-      transactions.
+      bank with Plaid. This process may take a few minutes. Once complete, you will be able to view
+      your transactions.
     </p>
     <p>
-      The transactions will be categorized once retrieved into one of three categories: Money-In,
-      Money-Out, and Transfer. These categories are determined by filters set by your admin, or
-      yourself. If there are no filters that the name of the transaction matches, it will be
-      categorized by the amount of the transaction. If the amount is positive, it will be
-      categorized as Money-In, and if it is negative, it will be categorized as Money-Out. You are
-      able to manually change the category of a transaction by selecting the dropdown menu on the
-      transaction and selecting a new category.
+      The transactions will be automatically categorized into one of three categories: Money-In,
+      Money-Out, and Transfer. These categories are determined by filters set by your admin or
+      yourself. If there are no filters that match the name of the transaction, it will be
+      categorized based on the amount of the transaction. If the amount is positive, it will be
+      categorized as Money-In, and if it is negative, it will be categorized as Money-Out. You can
+      also manually change the category of a transaction by selecting the dropdown menu on the
+      transaction and choosing a new category.
     </p>
   </div>
 );
