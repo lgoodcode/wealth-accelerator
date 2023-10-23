6fb770603bca384f3428545bec650449046d63c1
Lawrence Good <lawrence.good55@gmail.com>
2023-10-23 08:38:47 -0700

    update plaid ins removal

--- src/app/api/plaid/institutions/remove/[item_id]/route.ts
+++ src/app/api/plaid/institutions/remove/[item_id]/route.ts
@@ -36,21 +36,21 @@ async function deleteInstitution(_: Request, { params: { item_id } }: RemoveInst
     return NextResponse.json({ error: 'Failed to retrieve item' }, { status: 500 });
   }
 
+  // Revoke the access token first, if this fails, don't remove from database
+  try {
+    await plaidClient.itemRemove({ access_token: item.access_token });
+  } catch (error) {
+    console.error(error);
+    captureException(error);
+    return NextResponse.json({ error: 'Failed to remove item' }, { status: 500 });
+  }
+
   // Remove the item from the database
   const { error: supabaseError } = await supabase.from('plaid').delete().eq('item_id', item_id);
 
   if (supabaseError) {
     console.error(supabaseError);
     captureException(supabaseError);
-    return NextResponse.json({ error: 'Failed to remove item' }, { status: 500 });
-  }
-
-  // Revoke the access token
-  try {
-    await plaidClient.itemRemove({ access_token: item.access_token });
-  } catch (error) {
-    console.error(error);
-    captureException(error);
     return NextResponse.json({ error: 'Failed to remove item' }, { status: 500 });
   }
 
