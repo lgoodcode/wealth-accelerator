cb1ba386036783de75b906dd43ac180f2ee81df7
Lawrence Good <lawrence.good55@gmail.com>
2023-12-06 21:10:49 -0800

    properly trigger account update mode from the docs

--- src/lib/plaid/create-link-token.ts
+++ src/lib/plaid/create-link-token.ts
@@ -1,6 +1,12 @@
 import { plaidClient, createLinkTokenRequest } from './config';
 
-export const createLinkToken = async (id: string, access_token?: string) => {
-  const response = await plaidClient.linkTokenCreate(createLinkTokenRequest(id, access_token));
+export const createLinkToken = async (
+  id: string,
+  access_token?: string,
+  account_update = false
+) => {
+  const response = await plaidClient.linkTokenCreate(
+    createLinkTokenRequest(id, access_token, account_update)
+  );
   return response.data.link_token;
 };
