032e6d27bcc93de47ae91aa5d576d4c9a2e5077a
Lawrence Good <lawrence.good55@gmail.com>
2023-12-07 15:26:50 -0800

    Update initcap

--- src/lib/utils/initcap.ts
+++ src/lib/utils/initcap.ts
@@ -1,5 +1,17 @@
-export const initcap = (str: string) => {
-  return str.toLowerCase().replace(/(?:^|\s)\w/g, function (match) {
+/**
+ *
+ * If strict, only capitalize the first letter of each word and the rest is lowercase.
+ * Otherwise, capitalize the first letter of each word and the rest is unchanged.
+ *
+ * @param str
+ * @param strict
+ * @returns {string}
+ */
+export const initcap = (str: string, strict = true) => {
+  str = str.trim();
+  str = strict ? str.toLowerCase() : str;
+
+  return str.replace(/(?:^|\s)\w/g, function (match) {
     return match.toUpperCase();
   });
 };
