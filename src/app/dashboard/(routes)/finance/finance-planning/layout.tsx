f77635b3c09aec1fbeb977aacc781aba6bcd9b54
Lawrence Good <lawrence.good55@gmail.com>
2023-12-07 15:29:19 -0800

    Add route for account balances

--- src/app/dashboard/(routes)/finance/finance-planning/layout.tsx
+++ src/app/dashboard/(routes)/finance/finance-planning/layout.tsx
@@ -25,6 +25,10 @@ const sidebarNavItems: SidebarNavItem[] = [
     title: 'WAA Information',
     relativePath: '/waa-info',
   },
+  {
+    title: 'Balances',
+    relativePath: '/balances',
+  },
 ];
 
 interface PersonalFinanceLayoutProps {
