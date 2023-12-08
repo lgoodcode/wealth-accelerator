bfa6a94c31c60c15097e90f7dd969a29bbe60fd8
Lawrence Good <lawrence.good55@gmail.com>
2023-12-08 09:53:10 -0800

    Update constants

--- src/config/constants/react-query.ts
+++ src/config/constants/react-query.ts
@@ -1,3 +1,9 @@
-export const TRANSACTIONS_KEY = 'transactions';
-export const VISUALIZER_WAA_KEY = 'visualizer_waa';
-export const CREATIVE_CASH_FLOW_KEY = 'creative_cash_flow';
+// Used for the /banking transactions
+export const TRANSACTIONS_KEY = 'TRANSACTIONS';
+// Used for the CCF visualizer WAA data
+export const VISUALIZER_WAA_KEY = 'VISUALIZER_WAA';
+// Used for all the transactions in the CCF
+export const CREATIVE_CASH_FLOW_KEY = 'CREATIVE_CASH_FLOW';
+// Used for the CCF visualizer account balance entries
+export const CCF_BALANCE_ENTRIES_KEY = 'CFF_BALANCE_ENTRIES';
+export const ACCOUNT_BALANCES_ENTRIES_KEY = 'ACCOUNT_BALANCES_ENTRIES';
