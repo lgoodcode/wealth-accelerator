90dd3816a2a9f1b12784cf5b32700ea90abf2e58
Lawrence Good <lawrence.good55@gmail.com>
2024-01-16 20:31:59 -0800

    cleanup visualizer function

--- src/app/dashboard/(routes)/tools/creative-cash-flow/functions/creative-cash-flow-visualizer.ts
+++ src/app/dashboard/(routes)/tools/creative-cash-flow/functions/creative-cash-flow-visualizer.ts
@@ -17,17 +17,6 @@ interface CreativeCashFlowManagementArgs {
   balances_entries: BalancesEntryData[];
 }
 
-const filterTransactionsByDate = (
-  transactions: CcfTransaction[],
-  start_date: Date,
-  end_date: Date
-) => {
-  return transactions.filter((transaction) => {
-    const transaction_time = new Date(transaction.date);
-    return transaction_time >= start_date && transaction_time <= end_date;
-  });
-};
-
 export function visualizeCcf({
   interval,
   business_transactions,
@@ -90,25 +79,13 @@ export function visualizeCcf({
 
   const results = [];
   let collections = 0;
-  let business_overhead = 0;
-  let lifestyle_expenses = 0;
 
-  for (let i = 0; i < num_periods; i++) {
+  for (let i = 0; i < num_periods; i++, collections = 0) {
     // Get the start and end dates for this window
     const window_start_date = add(start_date, { [duration]: i });
     const _window_end_date = add(window_start_date, { [duration]: 1 });
     const window_end_date = _window_end_date > end_date ? end_date : _window_end_date;
-    // Get the transactions within this window
-    const window_business_transactions = filterTransactionsByDate(
-      business_transactions,
-      window_start_date,
-      window_end_date
-    );
-    const window_personal_transactions = filterTransactionsByDate(
-      personal_transactions,
-      window_start_date,
-      window_end_date
-    );
+
     // Get the WaaInfo within this range
     const window_waa_infos = waaInfos.filter((waaInfo) => {
       const waaInfoDate = new Date(waaInfo.date);
@@ -120,30 +97,6 @@ export function visualizeCcf({
       return entryDate >= window_start_date && entryDate <= window_end_date;
     });
 
-    for (const transaction of window_business_transactions) {
-      if (transaction.category === 'Money-In') {
-        collections -= transaction.amount;
-      } else {
-        business_overhead += transaction.amount;
-      }
-    }
-
-    business_overhead = Math.max(business_overhead, 0);
-
-    for (const transaction of window_personal_transactions) {
-      if (transaction.category === 'Money-Out') {
-        lifestyle_expenses += transaction.amount;
-      }
-    }
-
-    const lifestyle_expenses_tax = lifestyle_expenses * lifestyle_expenses_tax_rate;
-    const business_profit_before_tax =
-      collections - business_overhead - lifestyle_expenses - lifestyle_expenses_tax;
-    const tax_account =
-      business_profit_before_tax > 0 ? business_profit_before_tax * tax_account_rate : 0;
-    const waa =
-      business_profit_before_tax > 0 ? business_profit_before_tax - tax_account : collections / 10;
-
     results.push({
       range: {
         start: window_start_date,
@@ -154,21 +107,7 @@ export function visualizeCcf({
       balance: centsToDollars(
         window_balances_entries.reduce((acc, entry) => acc + entry.amount, 0)
       ),
-      // lifestyle_expenses: centsToDollars(lifestyle_expenses),
-      // lifestyle_expenses_tax: centsToDollars(lifestyle_expenses_tax),
-      // business_profit_before_tax: centsToDollars(business_profit_before_tax),
-      // business_overhead: centsToDollars(business_overhead),
-      // tax_account: centsToDollars(tax_account),
-      // waa: centsToDollars(waa),
-      // // waa: centsToDollars(waa),
-      // // daily_trend: daily_trend.map((trend) => centsToDollars(trend)),
-      // // weekly_trend: daily_trend.map((trend) => centsToDollars(trend * DAYS_IN_WEEK)),
-      // // yearly_trend: daily_trend.map((trend) => centsToDollars(trend * DAYS_IN_YEAR)),
     });
-
-    collections = 0;
-    business_overhead = 0;
-    lifestyle_expenses = 0;
   }
 
   return results;
